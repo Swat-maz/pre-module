c8db80ceb2e1caecc2cb3060540ad14cd38bc06b
Swat-maz <igor.mazur.selets@gmail.com>
2021-07-12 10:00:26 +0300

    done tsk 10

--- web/modules/custom/swat/src/Controller/CatsController.php
+++ web/modules/custom/swat/src/Controller/CatsController.php
@@ -18,48 +18,50 @@ class CatsController extends ControllerBase {
    *   Return markup array.
    */
   public function content() {
-
     $simpleform = \Drupal::formBuilder()->getForm('Drupal\swat\Form\CatsForm');
-//    $db = \Drupal::service('database');
-//    $query = $db->select('swat', 'cf');
-//    $query->fields('cf', array('name', 'email'));
-//    $result = $query->execute()->fetchAll();
     return [
       '#type' => 'markup',
       '#markup' => $this->t('Hello! You can add here a photo of your cat.'),
       $simpleform,
-      $this->show(1),
-//      $result,
+      $this->show(),
+
+      //      $this->report(),
     ];
   }
 
-  public function show($id) {
+  public function show() {
 
     $conn = Database::getConnection();
 
-    $query = $conn->select('swat', 'm')
-      ->condition('id', $id)
-      ->fields('m');
-    $data = $query->execute()->fetchAssoc();
-    $full_name = $data['name'];
-    $email = $data['email'];
-    $timestamp = $data['timestamp'];
-
-    $file = File::load($data['photo']);
-//    $picture = $file->getFileUri();
-    $picture[] = [
-      '#type' => 'image',
-      '#theme' => 'image_style',
-      '#style_name' => 'large',
-      '#uri' => $file->getFileUri(),
-    ];
-    return [
-      '#type' => 'markup',
-      '#markup' => "<h3>$full_name</h3><br>
-                    <p>$timestamp</p> <br>
+    $query = $conn->select('swat', 's');
+//    ->condition('id', $id);
+    $query->fields('s', ['id', 'name', 'email', 'timestamp', 'photo']);
+    $query->orderBy('s.timestamp', 'DESC');
+//    $query->range(0, 99);
+    $data = $query->execute()->fetchAllAssoc('id');
+    $data = json_decode(json_encode($data), TRUE);
+    $result = [];
+    foreach ($data as $value) {
+      $full_name = $value['name'];
+      $email = $value['email'];
+      $timestamp = $value['timestamp'];
+      $file = File::load($value['photo']);
+      //    $picture = $file->getFileUri();
+      $picture = [
+        '#type' => 'image',
+        '#theme' => 'image_style',
+        '#style_name' => 'large',
+        '#uri' => $file->getFileUri(),
+      ];
+      $result[] = [
+        '#type' => 'markup',
+        '#markup' => "<div class='swatshow'><h3>$full_name</h3>
                     <p>$email</p>",
-      $picture,
-    ];
+        $picture,
+        '#suffix' => "<p>$timestamp</p></div>",
+      ];
+    }
+    return $result;
   }
 
 }
